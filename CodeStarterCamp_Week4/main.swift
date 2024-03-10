c4ac629207b174044b45a25554902186b327d1ea
Jay <shinju1412@gmail.com>
2024-03-10 15:00:24 +0900

    Step2 - 야곰 피트니스, 지옥의 헬스 트레이닝

--- CodeStarterCamp_Week4/main.swift
+++ CodeStarterCamp_Week4/main.swift
@@ -7,6 +7,12 @@
 //
 
 var myBodyCondition: BodyCondition = BodyCondition(upperMuscleStrength: 30, lowerMuscleStrength: 30, muscularEndurance: 30, fatigue: 30)
+var myRoutine: Routine = Routine(name: "나의 루틴", activities: [sitUp, squat, running, activeRecovery])
 
-sitUp.startActivity(myBodyCondition)
-activeRecovery.startActivity(myBodyCondition)
+do {
+    try myRoutine.repeatRountine(myBodyCondition)
+} catch RoutineError.invalidInput {
+    print("잘못된 입력값입니다.")
+} catch RoutineError.maximumFatigueExceeded {
+    print("최대 피로도를 초과했습니다.")
+}
